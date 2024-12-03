68259aa086291f1c92c2ea048e0a7a9b5d15df61
parker <zppro@parkerdeMacBook-Air.local>
2024-12-03 13:51:40 +0800

    route-refine-done

--- app/routes/world.tsx
+++ app/routes/world.tsx
@@ -3,11 +3,12 @@ import { Outlet } from "@remix-run/react";
 import { SerializedWorld } from "@/worlds";
 import { useRootContext } from "~/hooks/use-context"
 import { redirect } from "@remix-run/node";
+import { appNavItems } from "~/data/nav";
 
 export default function Index() {
   const rootContext = useRootContext()
   return (
-    <Layout navMain={[]} worlds={rootContext.worlds ?? []}>
+    <Layout navMain={appNavItems} worlds={rootContext.worlds ?? []}>
       <Outlet context={rootContext} />
     </Layout>
   )
